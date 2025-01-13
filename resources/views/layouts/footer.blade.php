7bc8f88eaf8e6f364e50999798bf46212ccf7f95
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2025-01-13 12:26:26 +0530

    hindi

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
  <footer class="footer" style="bottom: 0; display: block; width: 100%;">
      <ul class="list-inline">
-         <li>2024 © vedraj.</li>
+         <li>{{ date('Y') }} © vaidraj.</li>
          <li><a href="#">Privacy</a></li>
          <li><a href="#">Terms</a></li>
          <li><a href="#">Help</a></li>
